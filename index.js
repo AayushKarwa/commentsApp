21541c4b8b2debf7ce7b6e237666ce0f80df899b
Ayush karwa <ayushkarwa18@gmail.com>
2024-07-18 17:49:49 +0530

    updated index.ejs and added some styles by bulma

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const methodOverride = require('method-override')
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine','ejs');
 app.use(methodOverride('_method'))
+const port = 3000;
 
 app.use(express.urlencoded({ extended: true }))
 let comments = [
@@ -73,6 +74,6 @@ app.get('/comments/:id/edit',(req,res)=>{
 })
 
 
-app.listen(3000,(req,res)=>{
+app.listen(port,(req,res)=>{
     console.log("LISTENING ON PORT 3000!")
 })
